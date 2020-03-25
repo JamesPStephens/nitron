b7831eed6cc21c98d0998ca2f200ae3627cad3be
James Stephens <jamesstephens@Jamess-MacBook-Pro.local>
2020-03-25 23:43:13 +0000

    Updates: More mobile friendly still needs some work. Favicon generated.

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = () => { PROGRESS_BAR()} ;
 
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
+const PROGRESS_BAR = () => {
+  const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
+
+const waypoint = new Waypoint({
+  element: document.getElementById('about'),
+  handler: () => {
+    const containerWidth = document.getElementById('about').clientWidth;
+    const whoweare = document.getElementById('whoweare');
+    whoweare.classList.add('animate_right');
+    const ourservices = document.getElementById('ourservices');
+    if (containerWidth <= 425) {
+      ourservices.classList.add('animate_left')
+    }
+    ourservices.classList.add('animate_top')
+  },
+  offset: '50%',
+})
+
+let TYPED = false;
+const TYPED_CONTACT = new Waypoint({
+  element: document.getElementById('contact'),
+  handler: () => {
+    if (!TYPED) {
+      const options = {
+        strings: ['Get in touch.'],
+        typeSpeed: 40
+      };
+      const typed = document.getElementById('typed');
+      new Typed('#typed', options);
+      typed.classList.add('animate_fadein');
+      TYPED = true;
+    }
+  },
+  offset: '25%',
+})
